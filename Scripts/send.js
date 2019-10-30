6ae1f0e7754f40871cc2c459372e70eb8cbe0644
Gwean <georgina.mondino1@gmail.com>
2019-10-30 10:39:33 -0300

    .

--- Scripts/send.js
+++ Scripts/send.js
@@ -1,6 +1,6 @@
 function send(){
     var http = new XMLHttpRequest();
-    var url = "https://soporte.hgtec.com.ar/api/http.php/tickets.json";
+    var url = "http://192.168.15.119:5000/send";
     var nombre = document.getElementById('Nombre').value;
     var email = document.getElementById('Email').value;
     var celular = document.getElementById('Celular').value;
@@ -8,7 +8,9 @@ function send(){
     var mensaje = document.getElementById('Mensaje').value;
     var archivos = document.getElementById('Archivos').value;
     http.open("POST", url, true);
+    http.setRequestHeader("Access-Control-Allow-Origin", "*");
     http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    http.setRequestHeader("X-API-Key", "DC0626B2B448D748889A17C90C3E0355");
     
     http.onreadystatechange = function() {
         if(http.readyState == 4 && http.status == 200) { 
@@ -17,18 +19,11 @@ function send(){
         }
     }
     let x = JSON.stringify({
-        "alert": true,
-        "autorespond": true,
-        "source": "API",
-        "name": "Angry User",
-        "email": "api@osticket.com",
-        "phone": "3185558634",
-        "subject": "Testing API",
-        "ip": "123.211.233.122",
-        "message": "data:text/html,MESSAGE <b>HERE</b>",
-        "attachments": [
-            {"file.txt": "data:text/plain;charset=utf-8,content"}
-        ]
+        "name": nombre,
+        "email": email,
+        "phone": celular,
+        "subject": asunto,
+        "message": mensaje,
     });
     console.log(x);
     http.send(x);
