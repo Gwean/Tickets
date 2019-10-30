6154f65fac17add46ebb9dc0f529b00297d1bbf0
Gwean <georgina.mondino1@gmail.com>
2019-10-30 12:55:31 -0300

    EnvioStates Listo

--- Scripts/send.js
+++ Scripts/send.js
@@ -1,5 +1,17 @@
+function showFormOnly() {
+    document.getElementById("ImagenEncabezado").style.display = "block";
+    document.getElementById("Formulario").style.display = "block";
+    document.getElementById("ElementosEnvio").style.display = "none";
+}
 
-function send(){
+function send() {
+
+    document.getElementById("ImagenEncabezado").style.display = "none";
+    document.getElementById("Formulario").style.display = "none";
+    document.getElementById("ElementosEnvio").style.display = "block";
+    document.getElementById("myDiv").style.display = "none";
+    document.getElementById("loaderContainer").style.display = "block";
+    
     var http = new XMLHttpRequest();
     var url = "http://192.168.15.119:5000/send";
     var nombre = document.getElementById('Nombre').value;
@@ -10,10 +22,12 @@ function send(){
     var archivos = document.getElementById('Archivos').files[0];
     http.open("POST", url, true);
 
-    http.onreadystatechange = function() {
-        if(http.readyState == 4 && http.status == 200) { 
-           //aqui obtienes la respuesta de tu peticion
-           alert(http.responseText);
+    http.onreadystatechange = function () {
+        console.log(http.readyState);
+        // COMPLETED 
+        if (http.readyState == 4) {
+            document.getElementById("loaderContainer").style.display = "none";
+            document.getElementById("myDiv").style.display = "block";
         }
     }
     form = new FormData();
@@ -24,8 +38,8 @@ function send(){
         "subject": asunto,
         "message": mensaje,
     });
-    form.append("data",x)
-    form.append("file",archivos)
+    form.append("data", x)
+    form.append("file", archivos)
     console.log(x);
     http.send(form);
 }
